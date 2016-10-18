c07b433989531d5faaf1d900e8f48427236a068e
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2016-10-19 00:00:06 +0200

    2nd attempt

--- GitPlayground/GitPlayground/EntryPoint.cs
+++ GitPlayground/GitPlayground/EntryPoint.cs
@@ -1,9 +1,20 @@
+using System;
+using System.Linq;
 using GitPlayground;
+using LibGit2Sharp;
 
 internal static class EntryPoint
 {
   public static void Main(string[] args)
   {
-    new GitRepositoryStatistics("E:\\grzes\\Documents\\GitHub\\tdd-toolkit\\").Gather();
+    //new GitRepositoryStatistics("E:\\grzes\\Documents\\GitHub\\tdd-toolkit\\").Gather();
+    using (var repo = new Repository("E:\\grzes\\Documents\\GitHub\\tdd-toolkit\\"))
+    {
+      foreach (var entry in repo.Index)
+      {
+        var count = repo.Commits.QueryBy(entry.Path).Count();
+        Console.WriteLine(count + " " + entry.Path);
+      }
+    }
   }
 }
